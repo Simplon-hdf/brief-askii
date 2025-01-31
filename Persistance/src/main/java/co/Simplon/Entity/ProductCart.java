ef3f5319635ed61c3c5b6c2e4997c901d3bac4bb
BborisB <b.boris.2001@gmail.com>
2025-01-31 11:01:19 +0100

    feat(entity): add Cart, Product, and ProductCart classes

---
+++ Persistance/src/main/java/co/Simplon/Entity/ProductCart.java
@@ -0,0 +1,50 @@
+package co.Simplon.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity
+public class ProductCart {
+
+    @Id
+    @Column(nullable = false, updatable = false, length = 50)
+    private String quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(final String quantity) {
+        this.quantity = quantity;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(final Cart cart) {
+        this.cart = cart;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(final Product product) {
+        this.product = product;
+    }
+
+}
