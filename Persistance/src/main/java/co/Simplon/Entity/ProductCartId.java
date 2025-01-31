622de1e3dc81ae52c384b6bb74b22ec6c74ca09f
YohanF1245 <yohan.ferdin@hotmail.fr>
2025-01-31 15:00:49 +0100

    fix(entity): improve ProductCartId with proper JPA implementation

--- Persistance/src/main/java/co/Simplon/Entity/ProductCartId.java
+++ Persistance/src/main/java/co/Simplon/Entity/ProductCartId.java
@@ -1,29 +1,50 @@
 package co.Simplon.Entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ProductCartId implements Serializable {
-	private Cart cart;
-	public Cart getCart() {
+	private static final long serialVersionUID = 1L;
+	
+	private String cart;    // Doit correspondre à l'ID de Cart
+	private String product; // Doit correspondre à l'ID de Product
+
+	public ProductCartId() {
+		// Constructeur par défaut requis par JPA
+	}
+
+	public ProductCartId(String cart, String product) {
+		this.cart = cart;
+		this.product = product;
+	}
+
+	public String getCart() {
 		return cart;
 	}
 
-	public void setCart(final Cart cart) {
+	public void setCart(String cart) {
 		this.cart = cart;
 	}
 
-	public Product getProduct() {
+	public String getProduct() {
 		return product;
 	}
 
-	public void setProduct(final Product product) {
+	public void setProduct(String product) {
 		this.product = product;
 	}
 
-	private Product product;
-	
-	public ProductCartId(Cart cart, Product product) {
-		this.cart = cart;
-		this.product = product;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ProductCartId that = (ProductCartId) o;
+		return Objects.equals(cart, that.cart) && 
+			   Objects.equals(product, that.product);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cart, product);
 	}
 }
